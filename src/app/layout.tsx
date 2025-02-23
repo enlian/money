3a003ea7c0d97f2a6d339e128e3acbd678e8bbcf
enlian <frejaenlian@gmail.com>
2025-02-23 19:43:34 +0900

    feat: 调整按钮组件样式，优化布局，更新页面最大宽度以提升用户体验

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,21 @@
-import './globals.css'
-import { AuthProvider } from './context/AuthContext';
+import "./globals.css";
+import { AuthProvider } from "./context/AuthContext";
 
 export const metadata = {
-  title: '我的资产统计',
-  description: '资产统计，并与当下热门资产做对比',
-}
+  title: "我的资产统计",
+  description: "资产统计，并与当下热门资产做对比",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body>
-        <AuthProvider>
-          {children}
-        </AuthProvider>
+      <body className="flex justify-center h-screen">
+        <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
-  )
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -55,7 +55,7 @@ const AssetsPage = () => {
   }
 
   return (
-    <div className="flex flex-col gap-4 p-6 bg-gray-50 h-full">
+    <div className="flex flex-col gap-4 p-6 bg-gray-50 h-full max-w-3xl">
       <div className="flex justify-end gap-3">
         <LoginModal />
         <AddAmountModal onSuccess={fetchData} />
