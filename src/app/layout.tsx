c1b3f498bc062a32e405286b43790ec229b702c2
enlian <frejaenlian@gmail.com>
2025-04-13 22:11:58 +0900

    feat: 修改页面标题为“资产统计”

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { QueryProvider } from "./providers/QueryProvider";
 import SessionProvider from "./providers/SessionProvider";
 
 export const metadata = {
-  title: "我的资产统计",
+  title: "资产统计",
   description: "资产统计，并与当下热门资产做对比",
 };
 
